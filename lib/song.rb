943282058c9c5590a62c4a8ab1f7bd7b13ff10f5
tonniegeetz <tonniegeetz@gmail.com>
2024-06-30 05:39:06 +0300

    from the db to ruby object code along

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,4 @@
 class Song
-
   attr_accessor :name, :album, :id
 
   def initialize(name:, album:, id: nil)
@@ -35,10 +34,10 @@ class Song
     SQL
 
     # insert the song
-    DB[:conn].execute(sql, self.name, self.album)
+    DB[:conn].execute(sql, name, album)
 
     # get the song ID from the database and save it to the Ruby instance
-    self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+    self.id = DB[:conn].execute('SELECT last_insert_rowid() FROM songs')[0][0]
 
     # return the Ruby instance
     self
@@ -49,4 +48,37 @@ class Song
     song.save
   end
 
+  def self.new_from_db(row)
+    # self.new is equivalent to Song.new
+    new(id: row[0], name: row[1], album: row[2])
+  end
+
+  sql = <<-SQL
+  SELECT *
+  FROM songs
+  SQL
+
+  def self.all
+    sql = <<-SQL
+      SELECT *
+      FROM songs
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      new_from_db(row)
+    end
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM songs
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      new_from_db(row)
+    end.first
+  end
 end
